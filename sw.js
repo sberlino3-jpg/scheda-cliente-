c2c4ddad97ad9996a900c3e50d3fae2f70338d05
sberlino3-jpg <sberlino3@gmail.com>
2025-09-20 19:05:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'scheda-cliente-v1';
+const OFFLINE_URLS = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        return response;
+      });
+    }).catch(() => caches.match('/index.html'))
+  );
+});
