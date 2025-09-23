4e40a0f263c35517ffb76530a22b9997717845f9
sberlino3-jpg <sberlino3@gmail.com>
2025-09-23 13:09:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'scheda-client-v2';
+const CACHE = 'scheda-client-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -8,20 +8,15 @@ const ASSETS = [
   './icons/icon-512.png',
   './icons/apple-touch-icon.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(self.clients.claim());
 });
-
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(res => res || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
   }
 });
